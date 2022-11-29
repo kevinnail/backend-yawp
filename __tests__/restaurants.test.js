8fc2a5d54ee2dc7492ede84a752273c7b9cf4749
kevinnail <fumalicious@gmail.com>
2022-11-28 18:46:49 -0800

    add GET /:id test, model, controller- test passing

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -16,7 +16,7 @@ describe('restaurant routes', () => {
     return setup(pool);
   });
 
-  it('GET /api/v1/restaurants should return a list of restaurants', async () => {
+  it.skip('GET /api/v1/restaurants should return a list of restaurants', async () => {
     const resp = await request(app).get('/api/v1/restaurants');
     expect(resp.status).toBe(200);
     expect(resp.body).toMatchInlineSnapshot(`
@@ -57,9 +57,42 @@ describe('restaurant routes', () => {
     `);
   });
 
-  it.skip('GET  /api/v1/restaurants/:restId should return a restaurant with a list of nested reviews', async () => {
+  it('GET  /api/v1/restaurants/:restId should return a restaurant with a list of nested reviews', async () => {
     const resp = await request(app).get('/api/v1/restaurants/1');
     expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Object {
+        "cost": 1,
+        "cuisine": "American",
+        "id": "1",
+        "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+        "name": "Pip's Original",
+        "reviews": Array [
+          Object {
+            "detail": "Best restaurant ever!",
+            "id": "1",
+            "restaurant_id": "1",
+            "stars": 5,
+            "user_id": "1",
+          },
+          Object {
+            "detail": "Terrible service :(",
+            "id": "2",
+            "restaurant_id": "1",
+            "stars": 1,
+            "user_id": "2",
+          },
+          Object {
+            "detail": "It was fine.",
+            "id": "3",
+            "restaurant_id": "1",
+            "stars": 4,
+            "user_id": "3",
+          },
+        ],
+        "website": "http://www.PipsOriginal.com",
+      }
+    `);
   });
 
   afterAll(() => {
